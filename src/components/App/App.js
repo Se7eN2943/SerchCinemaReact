61b9dd824a7c72b4d93ee537a438af0f497ec1ed
Se7eN <flip2943@gmail.com>
2022-01-27 18:05:50 +0300

    asdawd

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,52 @@
 import React, { Component } from 'react';
+import { Input, Spin, Alert } from 'antd';
 import FilmCardList from '../FilmCardList/FilmCardList'
-
-
-
-
+import CinemaService from '../services'
+import { format } from 'date-fns'
 
 
 
 
 export default class App extends Component {
 
+    movies = new CinemaService
+    state = { items: [], loaded: false, error: false }
 
+    getMovies = () => {
+        this.movies.apiResurses()
+            .then(apiObj => {
+                const elements = apiObj.results.map(item => {
+                    return (
+                        {
+                            key: item.id,
+                            name: item.title,
+                            date: format(new Date(item.release_date), 'PP'),
+                            overview: item.overview,
+                            img: `https://image.tmdb.org/t/p/original${item.poster_path}`
+                        }
+                    )
+                })
+                return this.setState({ items: elements, loaded: true })
+            })
+            .catch(this.onError)
+    }
 
+    onError = (err) => {
+        console.log(err)
+        this.setState({ loaded: true, error: true })
+    }
 
     render() {
-
-
-
+        const { error, loaded, items } = this.state
+        window.onload = this.getMovies
         return (
-            < FilmCardList />
+            <main className="filmCards">
+                <Input placeholder="Search" />
+                <h1>Популярное сегодня</h1>
+                {error &&  <Alert message="Не получилось загрузить данные =(" type="error" showIcon />}
+                {!loaded && <Spin />}
+                {!(loaded && error) && < FilmCardList card={items} />}
+            </main>
         );
     }
 }
--- src/components/FilmCardList/FilmCardList.js
+++ src/components/FilmCardList/FilmCardList.js
@@ -1,52 +1,31 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Input } from 'antd';
-import CinemaService from '../services'
 import FilmCard from '../FilmCard/FilmCard';
-import { format } from 'date-fns'
+
+
 
 export default class FilmCardList extends Component {
-  movies = new CinemaService
-  state = { items: [], }
 
-  getMovies() {
-    this.movies.apiResurses().then(apiObj => {
-      const elements = apiObj.results.map(item => {
-        return (
-          < FilmCard
-            key={item.id}
-            name={item.title}
-            date={format(new Date(item.release_date), 'PP')}
-            overview={item.overview}
-            img={`https://image.tmdb.org/t/p/original${item.poster_path}`}
-          />
-        )
-      })
-      return this.setState({ items: elements })
-    })
-  }
 
 
 
   render() {
-
-
-
-
+    const cardElements = this.props.card.map(item => {
+      return (
+        <FilmCard
+          key={item.key}
+          name={item.name}
+          date={item.date}
+          overview={item.overview}
+          img={item.img}
+        />
+      )
+    })
     return (
-      <main className="filmCards">
-        <Input placeholder="Search" onChange={() => this.getMovies()} />
-        <ul className="filmCardList">
-          {this.state.items}
-        </ul>
-      </main>
-
+      < ul className="filmCardList" >
+        {cardElements}
+      </ul >
     )
-
-
   }
-
-
-
 }
 
